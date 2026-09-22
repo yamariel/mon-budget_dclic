a579294f91a49ef6a0a30decabf87537ebd285ba
yamariel <ariel.yamien@epitech.eu>
2026-09-22 16:30:00 +0000

    feat: enumeration des type de transactions et donnée des catégories

---
+++ lib/data/category.dart
@@ -0,0 +1,10 @@
+import '../models/category.dart';
+
+final categories = [
+  Category(id: 'alimentation', name: 'Alimentation', colorHex: '#E06666'),
+  Category(id: 'transport', name: 'Transport', colorHex: '#2E75B6'),
+  Category(id: 'logement', name: 'Logement', colorHex: '#3CB371'),
+  Category(id: 'loisirs', name: 'Loisirs', colorHex: '#F6B93B'),
+  Category(id: 'sante', name: 'Santé', colorHex: '#8E7CC3'),
+  Category(id: 'autre', name: 'Autre', colorHex: '#9AA5B1'),
+];
---
+++ lib/enums/transactions_type.dart
@@ -0,0 +1,1 @@
+enum TransactionType { depense, revenu }
