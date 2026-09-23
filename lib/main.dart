08e76103c23d22de4580ac2d1bc044069c4a8c58
yamariel <ariel.yamien@epitech.eu>
2026-09-23 01:57:49 +0000

    feat: utilisation des providers pour l'affichage de la bonne page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'package:provider/provider.dart';
+
+import '../controllers/auth_controller.dart';
+import '../views/auth/login_screen.dart';
+import '../controllers/transaction_controller.dart';
+import '../views/home_screen.dart';
 import 'core/theme.dart';
 import 'firebase_options.dart';
 
@@ -15,10 +21,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      home: const Scaffold(body: Center(child: Text('Hello World!'))),
+    return ChangeNotifierProvider(
+      create: (_) => AuthController(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        title: "Budgeto",
+        home: _RootScreen(),
+      ),
+    );
+  }
+}
+
+class _RootScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = context.watch<AuthController>();
+    final user = auth.currentUser;
+    if (user == null) {
+      return LoginScreen();
+    }
+    return ChangeNotifierProvider(
+      create: (_) => TransactionController(user.uid),
+      child: HomeScreen(),
     );
   }
 }
