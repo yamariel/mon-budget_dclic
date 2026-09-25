a41dc4ceedd0d92fc710d9efe562f172437e0bf8
yamariel <ariel.yamien@epitech.eu>
2026-09-25 18:09:07 +0000

    feat: gestureDetectore pour les logo google et Apple

--- lib/widgets/square_title.dart
+++ lib/widgets/square_title.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 class SquareTitle extends StatelessWidget {
   final String pahtImage;
-  const SquareTitle({super.key, required this.pahtImage});
+  final Function()? onTap;
+  const SquareTitle({super.key, required this.pahtImage, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        border: Border.all(color: const Color(0xFFE2E8F0)),
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color(0xFFE2E8F0)),
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+        ),
+        child: Image.asset(pahtImage, height: 36),
       ),
-      child: Image.asset(pahtImage, height: 36),
     );
   }
 }
